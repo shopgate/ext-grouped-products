9d644f433313c0911cd65447400d24ef68eee6e9
Szymon Nowicki <sznowicki@eskalacja.com>
2018-06-22 15:29:32 +0200

    CCP-671 - extension portals

--- frontend/portals/GroupedProducts/components/Product/components/ProductPrice/index.jsx
+++ frontend/portals/GroupedProducts/components/Product/components/ProductPrice/index.jsx
@@ -4,49 +4,62 @@ import I18n from '@shopgate/pwa-common/components/I18n';
 import Price from '@shopgate/pwa-ui-shared/Price';
 import PriceStriked from '@shopgate/pwa-ui-shared/PriceStriked';
 import PriceInfo from '@shopgate/pwa-ui-shared/PriceInfo';
+import Portal from '@shopgate/pwa-common/components/Portal';
 import styles from './style';
+import {
+  GROUPED_PRODUCT_PRICE,
+  GROUPED_PRODUCT_PRICE_AFTER,
+  GROUPED_PRODUCT_PRICE_BEFORE,
+} from '../../../../../../constants/portals';
 
 /**
  * The Price component.
  * @param {Object} props The component props.
  * @return {JSX}
  */
-const ProductPrice = ({ price, renderLine }) => (
-  <Fragment>
-    <div className={renderLine ? styles.containerLine : styles.containerColumn}>
-      {(price.msrp > 0 && price.unitPrice !== price.msrp) && (
-        <div className={styles.priceStrikedContainer}>
-          <I18n.Text
-            className={styles.msrpLabel}
-            string="price.msrp"
-          />
-          <PriceStriked
-            value={price.msrp}
+const ProductPrice = (props) => {
+  const { price, renderLine } = props;
+  return (
+    <Fragment>
+      <Portal name={GROUPED_PRODUCT_PRICE_BEFORE} props={props} />
+      <Portal name={GROUPED_PRODUCT_PRICE} props={props}>
+        <div className={renderLine ? styles.containerLine : styles.containerColumn}>
+          {(price.msrp > 0 && price.unitPrice !== price.msrp) && (
+            <div className={styles.priceStrikedContainer}>
+              <I18n.Text
+                className={styles.msrpLabel}
+                string="price.msrp"
+              />
+              <PriceStriked
+                value={price.msrp}
+                currency={price.currency}
+                className={styles.priceStriked}
+              />
+            </div>
+          )}
+          {(!price.msrp && price.unitPriceStriked > 0) && (
+            <PriceStriked
+              value={price.unitPriceStriked}
+              currency={price.currency}
+              className={styles.priceStriked}
+            />
+          )}
+          <Price
+            className={styles.price}
             currency={price.currency}
-            className={styles.priceStriked}
+            discounted={!!price.discount}
+            unitPrice={price.unitPrice}
+            unitPriceMin={price.unitPriceMin}
           />
         </div>
-      )}
-      {(!price.msrp && price.unitPriceStriked > 0) && (
-        <PriceStriked
-          value={price.unitPriceStriked}
-          currency={price.currency}
-          className={styles.priceStriked}
-        />
-      )}
-      <Price
-        className={styles.price}
-        currency={price.currency}
-        discounted={!!price.discount}
-        unitPrice={price.unitPrice}
-        unitPriceMin={price.unitPriceMin}
-      />
-    </div>
-    {price.info && (
-      <PriceInfo text={price.info} className={styles.priceInfo} />
-    )}
-  </Fragment>
-);
+        {price.info && (
+          <PriceInfo text={price.info} className={styles.priceInfo} />
+        )}
+      </Portal>
+      <Portal name={GROUPED_PRODUCT_PRICE_AFTER} props={props} />
+    </Fragment>
+  );
+};
 
 ProductPrice.propTypes = {
   price: PropTypes.shape().isRequired,
