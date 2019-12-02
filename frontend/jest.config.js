5ffb221778568d6ce8ca8cf548ade184a575826d
Daniel Quintanilla <daniel.quintanilla@shopgate.com>
2019-12-02 08:23:56 -0600

    CCP-1794: Added config for test to run in standalone env

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
     '^.+\\.(css|scss)$': 'identity-obj-proxy',
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!(@shopgate/pwa-core|@shopgate/pwa-common|@shopgate/pwa-common-commerce|@shopgate/pwa-ui-ios|@shopgate/pwa-ui-material|@shopgate/pwa-ui-shared|css-spring)/).+\\.js$',
+    '/node_modules/',
   ],
   setupFiles: [
     ...jestConfig.setupFiles,
