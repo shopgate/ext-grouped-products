20a0288c529d08ae6f69f0715bb2fdd7d6644464
Daniel Quintanilla <daniel.quintanilla@shopgate.com>
2019-12-09 14:03:44 -0600

    CCP-1794: update jest config file

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,13 +2,4 @@ const jestConfig = require('@shopgate/pwa-unit-test/jest.config');
 
 module.exports = {
   ...jestConfig,
-  moduleNameMapper: {
-    '^.+\\.(css|scss)$': 'identity-obj-proxy',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/',
-  ],
-  setupFiles: [
-    ...jestConfig.setupFiles,
-  ],
 };
